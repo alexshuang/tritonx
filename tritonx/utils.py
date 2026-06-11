6d80c9ad1b737afc91803fea813d855c68b4a3dd
Shuang Huang <huangshuang@weituinfo.com>
2026-06-11 15:00:55 +0800

    Support profiling and benchmark with flops/s and bandwidth metrics

--- tritonx/utils.py
+++ tritonx/utils.py
@@ -1,5 +1,6 @@
 import triton
 import torch
+import functools
 
 try:
     from triton.backends.amd.testing import MPMark as _Mark
@@ -43,6 +44,42 @@ DTYPE_TO_TORCH_DTYPE = {
 }
 
 
+PEAK_PERF = {
+    "gfx938_cu72": {
+        'bf16': {
+            'flops': 383.3856e12,
+            'bw': 2.4e12,
+            'hz': 1.3e9,
+        },
+    },
+    "gfx938_cu64": {
+        'bf16': {
+            'flops': 340.7872e12,
+            'bw': 2.4e12, # same as cu72 ?
+            'hz': 1.3e9,
+        },
+    }
+}
+
+
+@functools.lru_cache
+def get_gpu_label():
+    target = triton.runtime.driver.active.get_current_target()
+    device = torch.cuda.current_device()
+    num_cu = torch.cuda.get_device_properties(device).multi_processor_count
+    return f"{target.arch}_cu{num_cu}"
+
+
+def get_peak_performance(torch_dtype):
+    gpu_label = get_gpu_label()
+    dtype = TORCH_DTYPE_TO_DTYPE[torch_dtype]
+    try:
+        return PEAK_PERF[gpu_label][dtype]
+    except Exception as e:
+        print(f"Undefined peak performance for {gpu_label}_{dtype}")
+        raise NotImplementedError
+
+
 def tensor_shape_dtype_str(t: torch.Tensor) -> str:
     shape_part = "x".join(str(d) for d in t.shape)
     dtype_abbr = TORCH_DTYPE_TO_DTYPE.get(t.dtype, str(t.dtype))
@@ -50,6 +87,12 @@ def tensor_shape_dtype_str(t: torch.Tensor) -> str:
 
 
 class Mark(_Mark):
+    def __init__(self, fn, benchmarks):
+        super().__init__(fn, benchmarks)
+        if 'flops_fn' in benchmarks.args:
+            self.flops_fn = benchmarks.args['flops_fn']
+        if 'bytes_fn' in benchmarks.args:
+            self.bytes_fn = benchmarks.args['bytes_fn']
 
     def _run(self, bench: triton.testing.Benchmark, save_path: str, show_plots: bool, print_data: bool, diff_col=False,
              save_precision=6, **kwrags):
@@ -62,7 +105,21 @@ class Mark(_Mark):
         y_max = [f'{x}-max' for x in bench.line_names]
         x_names = list(bench.x_names)
         prefix_names = ['*.pt']
-        df = pd.DataFrame(columns=prefix_names + x_names + y_mean + y_min + y_max)
+        perf_names = []
+        columns=prefix_names + x_names + y_mean + y_min + y_max
+        if self.flops_fn:
+            _names = ['TFLOPS', 'Peak TFLOPS', 'TFLOPS Utilization']
+            perf_names.extend(_names)
+            columns.extend(_names)
+        if self.bytes_fn:
+            _names = ['BW', 'Peak BW', 'BW Utilization']
+            perf_names.extend(_names)
+            columns.extend(_names)
+        if self.flops_fn and self.bytes_fn:
+            _names = ['AI', 'Compute Bound Ratio']
+            perf_names.extend(_names)
+            columns.extend(_names)
+        df = pd.DataFrame(columns=columns)
         pt_files = bench.args['pt']
         del bench.args['pt']
         for x, pt in zip(bench.x_vals, pt_files):
@@ -75,6 +132,9 @@ class Mark(_Mark):
             x_args = dict(zip(x_names, x))
 
             row_mean, row_min, row_max = [], [], []
+            tflops, peak_tflops, flops_utilization = [], [], []
+            bw, peak_bw, bw_utilization = [], [], []
+            ai, compute_bound_ratio = [], []
             for y in bench.line_vals:
                 ret = self.fn(**{'inputs': x_args}, **{bench.line_arg: y}, **bench.args, **kwrags)
                 try:
@@ -85,11 +145,38 @@ class Mark(_Mark):
                 row_min += [y_min]
                 row_max += [y_max]
 
+                if self.flops_fn or self.bytes_fn:
+                    from .replay import _move_to_device
+                    _x_args = _move_to_device(x_args, 'cpu')
+
+                if self.flops_fn:
+                    flops = self.flops_fn(_x_args)
+                    flops_per_sec = flops / (y_mean / 1000)
+                    peak_flops = get_peak_performance(_x_args['q_extend'].dtype)['flops']
+                    tflops += [flops_per_sec / 1e12]
+                    peak_tflops += [peak_flops / 1e12]
+                    flops_utilization += [flops_per_sec / peak_flops]
+
+                if self.bytes_fn:
+                    bytes = self.bytes_fn(_x_args)
+                    bytes_per_sec = bytes / (y_mean / 1000)
+                    peak_bytes = get_peak_performance(_x_args['q_extend'].dtype)['bw']
+                    bw += [bytes_per_sec / 1e12]
+                    peak_bw += [peak_bytes / 1e12]
+                    bw_utilization += [bytes_per_sec / peak_bytes]
+
+                if self.flops_fn and self.bytes_fn:
+                    _ai = flops / bytes
+                    ai += [_ai]
+                    compute_bound_ratio += [_ai / (peak_flops / peak_bytes)]
+
             from .replay import FloatTensor
             _x = [tensor_shape_dtype_str(o) if hasattr(o, 'dtype') else
                   o.get('type_str') if isinstance(o, FloatTensor) else o
                   for o in x]
-            df.loc[len(df)] = [pt.stem] + _x + row_mean + row_min + row_max
+            df.loc[len(df)] = [pt.stem] + _x + row_mean + row_min + row_max + \
+                                tflops + peak_tflops + flops_utilization + \
+                                bw + peak_bw + bw_utilization + ai + compute_bound_ratio
 
         # NYI:
         # if bench.plot_name:
@@ -116,7 +203,7 @@ class Mark(_Mark):
         #         plt.show()
         #     if save_path:
         #         plt.savefig(os.path.join(save_path, f"{bench.plot_name}.png"))
-        df = df[prefix_names + x_names + bench.line_names]
+        df = df[prefix_names + x_names + bench.line_names + perf_names]
         if diff_col and df.shape[1] == 2:
             col0, col1 = df.columns.tolist()
             df['Diff'] = df[col1] - df[col0]
